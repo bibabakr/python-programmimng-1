610685edcdd89f384823388e47aed52bcb74dcc6
bibabakr <habibabakribrahim@gmail.com>
2017-08-23 19:57:30 +0300

    Add files via upload

---
+++ ctof.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 18:51:18 2017
+
+@author: habiba
+"""
+
+def ctof (c):
+    f=20*c*9/5+32
+    return f
+print (ctof(10))
